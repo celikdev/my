fd2715f2cbdfdd47569b3f2c12c7d4660d1e7b36
Hakan <52114496+celikdev@users.noreply.github.com>
2024-06-25 02:37:46 +0300

    fix: type errors

--- src/components/AnalysisBoard.tsx
+++ src/components/AnalysisBoard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Chessground from "@react-chess/chessground";
 
 import "chessground/assets/chessground.base.css";
